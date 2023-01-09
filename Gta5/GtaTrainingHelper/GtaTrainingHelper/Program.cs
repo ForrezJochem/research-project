80c9d9e70506c8d21eede9a35126291b0d594011
ForrezJochem <55882018+ForrezJochem@users.noreply.github.com>
2023-01-10 00:57:32 +0100

    Setup of the GtaTrainingHelper project

---
+++ Gta5/GtaTrainingHelper/GtaTrainingHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿[assembly: Rage.Attributes.Plugin("GtaTrainingHelper", Description = "Setups game for training.", Author = "Jochem Forrez")]
+namespace GtaTrainingHelper
+{
+    using Rage;
+    public class Program
+    {
+        private static void Main()
+        {
+            Game.Console.Print("GtaTrainingHelper Starting.");
+        }
+    }
+}

79b18da90a28339ceb590522d2a9d2b0affa5bae
ForrezJochem <55882018+ForrezJochem@users.noreply.github.com>
2023-01-10 03:04:33 +0100

    Added methods (SetupRandomBlip, SetupBlip) added way to setup blips (waypoints) and updated the main loop

--- Gta5/GtaTrainingHelper/GtaTrainingHelper/Program.cs
+++ Gta5/GtaTrainingHelper/GtaTrainingHelper/Program.cs
@@ -7,9 +7,13 @@ namespace GtaTrainingHelper
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Threading;
 
     public class Program
     {
+        static Blip blip;
+        static Timer blipFixTimer;
+        static bool isTraining = false;
         static List<Location> locations = new List<Location> { };
         static string locationPath = Path.Combine(Environment.CurrentDirectory, @"./Plugins/Locations.json");
         private static void Main()
@@ -17,8 +21,27 @@ namespace GtaTrainingHelper
             Game.Console.Print("GtaTrainingHelper Starting.");
             Game.MaxWantedLevel = 0;
             locations = GetLocations();
+            blipFixTimer = new Timer(BlipFix, null, 0, 5000);
             while (true)
             {
+                if (Game.LocalPlayer.Character.CurrentVehicle)
+                {
+                    if (isTraining)
+                    {
+                        if (Game.LocalPlayer.Character.CurrentVehicle.Health < 500)
+                        {
+                            Game.Console.Print("Vehicle is damaged, resetting.");
+                            isTraining = false;
+                            GTHSetup();
+                        }
+                        else if ((int)blip.TravelDistanceTo(Game.LocalPlayer.Character.CurrentVehicle.Position) < 30)
+                        {
+                            Game.Console.Print("GtaTrainingHelper: Arrived at location.");
+                            isTraining = false;
+                            GTHSetup();
+                        }
+                    }
+                }
                 GameFiber.Yield();
             }
         }
@@ -104,6 +127,49 @@ namespace GtaTrainingHelper
             Game.LocalPlayer.Character.WarpIntoVehicle(vehicle, -1);
             Game.LocalPlayer.Character.CanFlyThroughWindshields = false;
             Game.LocalPlayer.Character.CanBePulledOutOfVehicles = false;
+            GTHSetupRandomBlip();
+            isTraining = true;
+        }
+
+        // setup random blip (waypoint)
+        [Rage.Attributes.ConsoleCommand]
+        public static void GTHSetupRandomBlip()
+        {
+            if (blip.Exists())
+                blip.Delete();
+            Random random = new Random();
+            blip = new Blip(locations[random.Next(locations.Count)].Position);
+            // to check if blip is not to close to startin location
+            while ((int)blip.TravelDistanceTo(Game.LocalPlayer.Character.Position) < 60)
+            {
+                blip.Delete();
+                blip = new Blip(locations[random.Next(locations.Count)].Position);
+            }
+            blip.IsRouteEnabled = true;
+        }
+
+        // setup blip (waypoint) with name
+        [Rage.Attributes.ConsoleCommand]
+        public static void GTHSetupBlip(string location)
+        {
+            if (blip.Exists())
+                blip.Delete();
+            foreach (Location loc in locations)
+            {
+                if (loc.LocationName.ToLower() == location.ToLower())
+                {
+                    blip = new Blip(loc.Position);
+                    blip.IsRouteEnabled = true;
+                }
+            }
+        }
+        // weird bug that removes the blip after a while
+        private static void BlipFix(object state)
+        {
+            if (blip.Exists())
+            {
+                blip.IsRouteEnabled = true;
+            }
         }
     }
 }
